9416d93944aa2179366853e6841037d1494926ee
Amit <amitko1351@gmail.com>
2020-04-05 19:03:50 +0300

    Add Math class and its methods

---
+++ MathModule.py
@@ -0,0 +1,39 @@
+class Math:
+    """
+    Contain math operations 
+    """
+    @staticmethod
+    def add(num1, num2):
+        """
+        The function do addition operation on the two number 
+        num1 + num2
+        :return: the addition result
+        """
+        return num1 + num2
+
+    @staticmethod
+    def sub(num1, num2):
+        """
+        The function do subtraction operation on the two number 
+        num1 - num2
+        :return: the subtraction result
+        """
+        return num1 - num2
+
+    @staticmethod
+    def div(num1, num2):
+        """
+        The function do division operation on the two number 
+        num1 / num2
+        :return: the division result as float type
+        """
+        return float(num1) / num2
+
+    @staticmethod
+    def mul(num1, num2):
+        """
+        The function do multiplication operation on the two number 
+        num1 / num2
+        :return: the multiplication result
+        """
+        return num1 * num2
