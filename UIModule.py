d92f9bd65fc4d4522ea49add014227c6cf54a347
Amit <amitko1351@gmail.com>
2020-04-15 16:51:27 +0300

    Add the UI class and its methods

---
+++ UIModule.py
@@ -0,0 +1,111 @@
+# Import Section
+import json
+import sys
+
+# Global Section
+MARGIN = 7
+
+
+# Code Section
+
+class UI:
+    def __init__(self, calculators, calculator_methods_conf_file):
+        self.__calculators = calculators
+        self.__operate_calculator = None
+        # Read form the configuration file given
+        with open(calculator_methods_conf_file) as json_conf_file:
+            self.__calculator_methods = json.load(json_conf_file)
+
+    def __show_open_message(self):
+        """
+        Print the open message
+        """
+        welcome_message = "Welcome to the Cool Calculator"
+        self.print_header(welcome_message)
+
+    def print_header(self, header):
+        """
+        The function print thr header in the format
+        --------------------------------------------
+               Welcome to the Cool Calculator
+        --------------------------------------------
+        :param header: the header to print
+        """
+        global MARGIN
+        border_size = MARGIN * 2 + len(header)
+        print("-" * border_size)
+        print((" " * MARGIN) + header)
+        print("-" * border_size)
+
+    def main_loop(self):
+        """
+        The function active the loop of the UI
+        """
+        self.__show_open_message()
+        while True:
+            chose_calculator = self.__choose_calculator()
+            chose_calculator_method = self.__choose_calculator_method()
+            # Loop until exit thr calculator
+            self.__active_calculator(chose_calculator, chose_calculator_method)
+
+    def __choose_calculator(self):
+        """
+        The function let the user choose the calculator type 
+        :return: the type of calculator choose
+        """
+        while True:
+            self.print_header("Please enter the calculator you want to use")
+            # Display options
+            for index in range(len(self.__calculators)):
+                print("Enter - {} for {}.".format(str(index), self.__calculators[index].__doc__()))
+            # Get the choice
+            index_chose_calculator = input("Your choice ->")
+            # Check validation
+            if index_chose_calculator.isdigit() and len(self.__calculators) > int(index_chose_calculator) >= 0:
+                return self.__calculators[int(index_chose_calculator)]
+            # Invalid input
+            print("Invalid input, please make to sure to enter the correct number", file=sys.stderr)
+
+    def __choose_calculator_method(self):
+        """
+        The function let the user choose the method of calculation
+        :return: the method of calculation chose
+        """
+        while True:
+            self.print_header("Please enter the method of calculation you want to use")
+            # Display options
+            calculation_methods_names = list(self.__calculator_methods.keys())
+            for index in range(len(calculation_methods_names)):
+                print("Enter - {} for {}.".format(str(index), self.__calculator_methods[calculation_methods_names[
+                    index]]["doc"]))
+            # Get the choice
+            index_chose_method = input("Your choice ->")
+            # Check validation
+            if index_chose_method.isdigit() and len(calculation_methods_names) > int(index_chose_method) >= 0:
+                return calculation_methods_names[int(index_chose_method)]
+            # Invalid input
+            print("Invalid input, please make to sure to enter the correct number", file=sys.stderr)
+
+    def __active_calculator(self, chose_calculator, chose_calculator_method):
+        """
+        The function activate the calculator chose 
+        :param chose_calculator: the class of the chose calculator
+        :param chose_calculator_method: the name of the chose calculator method 
+        """
+        # Create the Calculator chose
+        self.__operate_calculator = chose_calculator(
+            self.__calculator_methods[chose_calculator_method]["operations_functions"],
+            self.__calculator_methods[chose_calculator_method]["operations_order"])
+        self.print_header("Calculator activated")
+        while True:
+            print("Please enter the math expression to calculate or exit to change calculator")
+            experssion = input("->")
+            if experssion == "exit":
+                break
+            try:
+                print("result -> {}".format(str(self.__operate_calculator.evaluate(experssion))))
+            except SyntaxError as e:
+                print(e.msg)
+            except ZeroDivisionError as e:
+                print("Can't divided by zero")
+
